2aeb7745a6c9bbb5049d52dba0ffed559ad517dc
Adrian Cunanan <adrian.cunanan@me.com>
2013-04-05 16:41:23 -0400

    Exercise Completed

--- Degrees/main.c
+++ Degrees/main.c
@@ -7,12 +7,26 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+// Initialize lastTmeperature to 50
+static float lastTemperature = 50.0;
+
+float fahrenheitFromCelsius(float cel)
+{
+    lastTemperature = cel;
+    float fahr = cel * 1.8 + 32.0;
+    printf("%f Celsius is %f Fahrenheit\n", cel, fahr);
+    return fahr;
+}
 
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    float freezeInC = 0;
+    float freezeInF = fahrenheitFromCelsius(freezeInC);
+    printf("Water freezes at %f degrees Fahrenheit\n", freezeInF);
+    printf("The last temperature converted was %f\n", lastTemperature);
+    return EXIT_SUCCESS;
 }
 
